b0cc95fbea718e29eb9b5e16acbcae30056bef48
Maria Shapiro <mariaashapiro@gmail.com>
2019-09-24 13:29:30 -0400

    added region class and techlevel enum and added some attributes to player class

---
+++ src/Region.java
@@ -0,0 +1,14 @@
+public class Region {
+
+    private int x;
+    private int y;
+    private TechLevel techLevel;
+    private String name;
+
+    public Region(int x, int y, TechLevel techLevel, String name) {
+        this.x = x;
+        this.y = y;
+        this.techLevel = techLevel;
+        this.name = name;
+    }
+}
