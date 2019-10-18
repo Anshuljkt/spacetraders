d113fad6ae3e64cb06908f20670a0a3dac7407a2
Maria Shapiro <mariaashapiro@gmail.com>
2019-10-18 16:01:13 -0400

    added constuctor for shiptype and instance attributes

--- src/Ship.java
+++ src/Ship.java
@@ -14,6 +14,7 @@ public enum Ship {
     private int shipHealth;
 
 
+
     Ship(int cargoSpace, int fuelCapacity, int shipHealth) {
         this.cargoSpace = cargoSpace;
         this.fuelCapacity = fuelCapacity;
